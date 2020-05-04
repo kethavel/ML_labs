50d35fe4fd3364b12724fa2056bf166fa81a1390
kethavel <kethavel.me@gmail.com>
2020-05-04 15:29:08 +0300

    lab1_3 is done

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,12 +1,21 @@
 from sklearn import metrics
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
-from random import sample
 import matplotlib.pyplot as plt
 
 
 def makeDataAndTarget(array: np.ndarray, dictionary: dict) -> (np.ndarray, np.ndarray):
-    """Replace given str values in array with int ones"""
+    """Заменяет значения в соответствии со словарем dictionary, выделяет последний столбец в target,
+    приводит значения к float
+
+    :param array: массив исходных данных
+    :param dictionary: словарь, в соответствии с которым заменяются значения
+    :returns: data: массив значений (столбцы array без последнего)
+              target: массив соответствующих классов (последний столбец array)
+    """
     target = array[:, -1].copy()
     data = array[:, 0:-1].copy()
     with np.nditer(target, op_flags=['readwrite']) as it:
@@ -22,71 +31,42 @@ def makeDataAndTarget(array: np.ndarray, dictionary: dict) -> (np.ndarray, np.nd
     return data, target
 
 
-def divideDataset(dataset: np.ndarray, targetDataset: np.ndarray, testPercent: float) -> \
-        (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
-    """Разделяем выборку на тренировочную и тестовую случайным образом
-    :param dataset: данные, которые нужно разделить
-    :param targetDataset: массив классов, соответствующий dataset
-    :param testPercent: количество тестовых данных, выраженное в доле от общего количества данных
-    :returns: trainingData, testData, trainingTarget, testTarget - соответствующие массивы разделденных данных
-    """
-    if testPercent > 1 or testPercent < 0:
-        raise ValueError('testPercent has to be in between 0 and 1')
-    if dataset.shape[0] != targetDataset.shape[0]:
-        raise ValueError('dataset and targetDataset has different length')
-
-    dataLength = dataset.shape[0]
-    testLength = int(dataLength * testPercent)
-    trainingLength = dataLength - testLength
-    testNumberSequence = sample(range(dataLength), k=testLength)
-    # создаем массивы под данные
-    trainingData = np.ndarray(shape=(trainingLength,) + dataset.shape[1:], dtype=float)
-    trainingTarget = np.ndarray(shape=(trainingLength,) + targetDataset.shape[1:], dtype=float)
-    testData = np.ndarray(shape=(testLength,) + dataset.shape[1:], dtype=float)
-    testTarget = np.ndarray(shape=(testLength,) + targetDataset.shape[1:], dtype=float)
-    # заполняем
-    testIndex = 0
-    dataIndex = 0
-    for i in range(dataLength):
-        if i in testNumberSequence:
-            testData[testIndex] = dataset[i]
-            testTarget[testIndex] = targetDataset[i]
-            testIndex += 1
-        else:
-            trainingData[dataIndex] = dataset[i]
-            trainingTarget[dataIndex] = targetDataset[i]
-            dataIndex += 1
-    return trainingData, testData, trainingTarget, testTarget
-
-
 def lab1_1(dataset: np.ndarray, targetDataset: np.ndarray, dataName: str) -> None:
     """1 пункт лабораторной работы: Исследуйте, как объем обучающей выборки и количество тестовых данных,
     влияет на точность классификации в датасетах с помощью наивного Байесовского классификатора. Постройте графики
-    зависимостей точности на обучающей и тестовой выборках в зависимости от их соотношения. """
+    зависимостей точности на обучающей и тестовой выборках в зависимости от их соотношения.
+
+    :param dataset: даные для обучения
+    :param targetDataset: соответствующие классы
+    :param dataName: имя данных, с которыми работаем"""
     listXPoints = []
     listYPoints = []
     for i in range(1, 10):
         testPercent = i / 10
-        data, test, target, testTarget = divideDataset(dataset, targetDataset, testPercent)
+        x_train, x_test, y_train, y_test = train_test_split(dataset, targetDataset, test_size=testPercent)
         gnb = GaussianNB()
-        gnb.fit(data, target)
-        y_pred = gnb.predict(test)
+        gnb.fit(x_train, y_train)
+        y_pred = gnb.predict(x_test)
+        accuracy = accuracy_score(y_test, y_pred)
         listXPoints.append(testPercent)
-        listYPoints.append(float((testTarget != y_pred).sum()) / float(test.shape[0]))
+        listYPoints.append(accuracy)
     plt.plot(listXPoints, listYPoints, scalex=False, scaley=False)
     plt.xlabel('Test data part out of a total amount')
-    plt.ylabel('Part of mislabeled points')
-    plt.title('Accuracy of Bayes classifier for ' + dataName)
+    plt.ylabel('Part of correct prediction')
+    plt.title('Accuracy of Bayes classifier for ' + dataName + ' (more is better)')
     plt.show()
 
 
 def lab1_2(featuresMinus1: np.ndarray, featuresPlus1: np.ndarray) -> None:
-    """2 пункт лабораторной работы: 2.	Сгенерируйте 100 точек с двумя признаками X1 и X2 в соответствии с нормальным
+    """2 пункт лабораторной работы: Сгенерируйте 100 точек с двумя признаками X1 и X2 в соответствии с нормальным
     распределением так, что одна и вторая часть точек (класс -1 и класс 1) имеют параметры: мат. ожидание X1,
     мат. ожидание X2, среднеквадратические отклонения для обеих переменных, соответствующие вашему варианту (указан в
     таблице). Построить диаграммы, иллюстрирующие данные. Построить Байесовский классификатор и оценить качество
     классификации с помощью различных методов (точность, матрица ошибок, ROС и PR-кривые). Является ли построенный
-    классификатор «хорошим»? """
+    классификатор «хорошим»?
+
+    :param featuresMinus1: массив признаков X1 и X2 для класса -1
+    :param featuresPlus1: массив признаков X1 и X2 для класса +1"""
     # сторим диаграмму, иллюстрирующую данные
     plt.plot(featuresMinus1[:, 0], featuresMinus1[:, 1], 'ro')
     plt.plot(featuresPlus1[:, 0], featuresPlus1[:, 1], 'bo')
@@ -98,25 +78,90 @@ def lab1_2(featuresMinus1: np.ndarray, featuresPlus1: np.ndarray) -> None:
     # строим классификатор
     dataset = np.concatenate((featuresPlus1, featuresMinus1))
     targetDataset = np.array([1 for i in range(featuresPlus1.shape[0])] + [-1 for i in range(featuresMinus1.shape[0])])
-    data, test, target, testTarget = divideDataset(dataset, targetDataset, 0.5)
+    x_train, x_test, y_train, y_test = train_test_split(dataset, targetDataset, test_size=0.33)
     gnb = GaussianNB()
-    gnb.fit(data, target)
+    gnb.fit(x_train, y_train)
     # оцениваем качество
-    prediction: np.ndarray = gnb.predict(test)
+    prediction: np.ndarray = gnb.predict(x_test)
     # точность
-    print("Number of mislabeled points out of a total {} points : {} , percent of mistakes: {}".format(
-        test.shape[0],
-        (testTarget != prediction).sum(),
-        float((testTarget != prediction).sum()) / float(test.shape[0])))
+    print("accuracy = {}".format(accuracy_score(y_test, prediction)))
     # матрица ошибок
-    metrics.plot_confusion_matrix(gnb, test, testTarget)
+    metrics.plot_confusion_matrix(gnb, x_test, y_test)
+    plt.title('Confusion matrix')
     plt.show()
     # ROС
-    metrics.plot_roc_curve(gnb, test, testTarget)
+    metrics.plot_roc_curve(gnb, x_test, y_test)
+    plt.title('ROC curve')
     plt.show()
     # PR
-    metrics.plot_precision_recall_curve(gnb, test, testTarget)
+    metrics.plot_precision_recall_curve(gnb, x_test, y_test)
+    plt.title('PR curve')
     plt.show()
+
+
+def lab1_3(dataset: np.ndarray, targetDataset: np.ndarray, dataName: str) -> None:
+    """3 пункт:	Постройте классификатор на основе метода k ближайших соседей для обучающего множества Glass (glass.csv).
+    a.	Постройте графики зависимости ошибки классификации от количества ближайших соседей.
+    b.	Определите подходящие метрики расстояния и исследуйте, как тип метрики расстояния влияет на точность
+    классификации.
+    c.	Определите, к какому типу стекла относится экземпляр с характеристиками:
+    RI =1.516 Na =11.7 Mg =1.01 Al =1.19 Si =72.59 K=0.43 Ca =11.44 Ba =0.02 Fe =0.1
+    """
+
+    # график зависимости точности от количества соседей
+    listXPoints = []
+    listYPoints = []
+    for i in range(1, 10):
+        classifier = KNeighborsClassifier(n_neighbors=i)
+        x_train, x_test, y_train, y_test = train_test_split(dataset, targetDataset, test_size=0.33)
+        classifier.fit(x_train, y_train)
+        y_predicted = classifier.predict(x_test)
+        accuracy = accuracy_score(y_test, y_predicted)
+        listXPoints.append(i)
+        listYPoints.append(accuracy)
+    plt.plot(listXPoints, listYPoints, scaley=False)
+    plt.xlabel('Amount of neighbors')
+    plt.ylabel('Part of correct prediction')
+    plt.title('Accuracy of K Neighbors Classifier for ' + dataName + '(more is better)')
+    plt.show()
+
+    # точность в зависимости от метрики
+    print('Metrics accuracy:')
+    metrics = ('euclidean', 'manhattan', 'chebyshev')
+    for metric in metrics:
+        classifier = KNeighborsClassifier(metric=metric)
+        x_train, x_test, y_train, y_test = train_test_split(dataset, targetDataset, test_size=0.33)
+        classifier.fit(x_train, y_train)
+        y_predicted = classifier.predict(x_test)
+        accuracy = accuracy_score(y_test, y_predicted)
+        print('accuracy for {} metric: {}'.format(metric, accuracy))
+
+    classifier = KNeighborsClassifier(metric='minkowski', p=3)
+    x_train, x_test, y_train, y_test = train_test_split(dataset, targetDataset, test_size=0.33)
+    classifier.fit(x_train, y_train)
+    y_predicted = classifier.predict(x_test)
+    accuracy = accuracy_score(y_test, y_predicted)
+    print('accuracy for minkowski metric with p = 3: {}'.format(accuracy))
+
+    # определить тип стекла
+    RI = 1.516
+    Na = 11.7
+    Mg = 1.01
+    Al = 1.19
+    Si = 72.59
+    K = 0.43
+    Ca = 11.44
+    Ba = 0.02
+    Fe = 0.1
+    classifier = KNeighborsClassifier()
+    x_train, x_test, y_train, y_test = train_test_split(dataset, targetDataset, test_size=0.33)
+    classifier.fit(x_train, y_train)
+    y_predicted = classifier.predict(x_test)
+    accuracy = accuracy_score(y_test, y_predicted)
+    unknownGlass = np.array([RI, Na, Mg, Al, Si, K, Ca, Ba, Fe], ndmin=2)
+    prediction = classifier.predict(unknownGlass)
+    print('Unknown glass is {0[0]} class. Accuracy: {1}'.format(prediction, accuracy))
+    return
 
 
 if __name__ == '__main__':
@@ -126,12 +171,15 @@ if __name__ == '__main__':
                                                                                                 'x': 2,
                                                                                                 'o': 3,
                                                                                                 'b': 4})
-    lab1_1(tick_tack_toe_data, tick_tack_toe_target, 'tic_tac_toe.txt')
+
+    print('Lab1.1: tic tac toe')
+    # lab1_1(tick_tack_toe_data, tick_tack_toe_target, 'tic_tac_toe.txt')
 
     spam_csv = np.loadtxt('data/spam.csv', delimiter=',', dtype=str)[1:, 1:]
     spam_data, spam_target = makeDataAndTarget(spam_csv, dictionary={'"spam"': 1,
                                                                      '"nonspam"': -1})
-    lab1_1(spam_data, spam_target, 'spam.csv')
+    print('Lab1.1: spam')
+    # lab1_1(spam_data, spam_target, 'spam.csv')
 
     # Вариант	                        5
     # Матем. ожид. X1 (класс -1)	    14
@@ -147,4 +195,18 @@ if __name__ == '__main__':
     # Вывод в массив размером size 50, 2
     featuresClassMinus1 = np.random.normal(loc=(14, 10), scale=(4, 4), size=(50, 2))
     featuresClassPlus1 = np.random.normal(loc=(16, 10), scale=(1, 1), size=(50, 2))
-    lab1_2(featuresClassMinus1, featuresClassPlus1)
+    print('Lab1.2:')
+    # lab1_2(featuresClassMinus1, featuresClassPlus1)
+
+    glass_csv = np.loadtxt('data/glass.csv', delimiter=',', dtype=str)[1:, 1:]
+    glassData, glassTarget = makeDataAndTarget(glass_csv, dictionary={'"1"': 1,
+                                                                      '"2"': 2,
+                                                                      '"3"': 3,
+                                                                      '"4"': 4,
+                                                                      '"5"': 5,
+                                                                      '"6"': 6,
+                                                                      '"7"': 7,
+                                                                      })
+    print('Lab1.3:')
+    lab1_3(glassData, glassTarget, 'glass.csv')
+
